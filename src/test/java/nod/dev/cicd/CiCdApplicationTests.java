eaae635c399e9cdbf79e7bd3bd79ced6c22039b0
DinuwanB <dinubhagya97@gmail.com>
2024-03-01 17:18:11 +0530

    first commit

---
+++ src/test/java/nod/dev/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package nod.dev.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
